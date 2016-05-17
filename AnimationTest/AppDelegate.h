4d303d77444e6e140c2c23a0f99b7727376c81e7
Michael LIN <172345927@qq.com>
2016-05-17 11:50:01 +0800

    Initial Commit

---
+++ AnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTest
+//
+//  Created by Michaelin on 16/5/17.
+//  Copyright © 2016年 Michaelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by Michaelin on 16/5/17.
+//  Copyright © 2016年 Michaelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
